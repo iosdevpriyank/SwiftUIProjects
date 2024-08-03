cfd8b53d1de55cc69b4e3535a6385d18ee3c4d20
Priyank Gandhi <iosdev.priyank@gmail.com>
2024-08-03 15:26:34 +0530

    My first SwiftUI Project
    My first SwiftUI Project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Akshat Gandhi on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
