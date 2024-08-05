eaa93afcff3e5ee7371a6b09d5390e5274acf992
Priyank Gandhi <iosdev.priyank@gmail.com>
2024-08-05 15:23:45 +0530

    Guess The Flag New Project
    Guess The Flag New Project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Akshat Gandhi on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
