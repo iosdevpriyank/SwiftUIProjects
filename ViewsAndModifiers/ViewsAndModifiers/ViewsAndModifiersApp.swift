9d3a862ccf8d0febb2ad12ce6d814b6c4f79f038
Priyank Gandhi <iosdev.priyank@gmail.com>
2024-08-11 02:11:18 +0530

    Project 3 Created
    Project 3 Created

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Akshat Gandhi on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
