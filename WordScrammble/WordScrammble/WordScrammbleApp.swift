f0d5e627a597b632d8f3642bbb540e1fff8f6d01
Priyank Gandhi <iosdev.priyank@gmail.com>
2024-08-18 08:39:16 +0530

    Project 5 Finish
    Project 5 Finish

---
+++ WordScrammble/WordScrammble/WordScrammbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrammbleApp.swift
+//  WordScrammble
+//
+//  Created by Akshat Gandhi on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrammbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
